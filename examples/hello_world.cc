6618f46c9b163422944a803f01bad77088aaee1f
Kevin Gliewe <kevingliewe@gmail.com>
2023-09-12 15:59:14 +0000

    Use std::vector instead of std::tuple to add handlers during runtime.

--- examples/hello_world.cc
+++ examples/hello_world.cc
@@ -6,73 +6,79 @@
 
 // Define a request.
 struct SayHello : holden::request<void> {
-  // the output stream through which the handler should "say hello."
-  std::ostream& output_stream;
+    // the output stream through which the handler should "say hello."
+    std::ostream& output_stream;
 
-  SayHello(std::ostream& out) : output_stream(out) {}
+    SayHello(std::ostream& out) : output_stream(out) {}
 };
 
 enum class FirstImpression { Good, Great, Stupendous, };
 
 std::ostream& operator<<(std::ostream& os, FirstImpression i) {
-  switch (i) {
-   case FirstImpression::Good:       os << "good!";
-   case FirstImpression::Great:      os << "great!!";
-   case FirstImpression::Stupendous: os << "stupendous!!!";
-   default: os << "off the charts!";
-  }
-  
-  return os;
+    switch (i) {
+    case FirstImpression::Good:       os << "good!";
+    case FirstImpression::Great:      os << "great!!";
+    case FirstImpression::Stupendous: os << "stupendous!!!";
+    default: os << "off the charts!";
+    }
+
+    return os;
 }
 
 // Or we can skip the `request<>` interface and add `response_type` manually.
 struct SayGoodbye {
-  // Let mediator know what return type to infer.
-  using response_type = FirstImpression;
+    // Let mediator know what return type to infer.
+    using response_type = FirstImpression;
 
-  // the output stream through which the handler should "say hello."
-  std::ostream& output_stream;
+    // the output stream through which the handler should "say hello."
+    std::ostream& output_stream;
 
-  SayGoodbye(std::ostream& out) : output_stream(out) {}
+    SayGoodbye(std::ostream& out) : output_stream(out) {}
 };
 
-// Configure a request handler.
-class Speaker
-  : holden::request_handler<SayHello>
-  , holden::request_handler<SayGoodbye> {
- public:
-  // Say hello on the provided output stream.
-  void handle(const SayHello& r) {
-    r.output_stream << "Hello, world!\n";
-  }
+// Configure a request handlers.
 
-  // Say goodbye on the provided output stream, and return how the interaction 
-  // went.
-  FirstImpression handle(const SayGoodbye& r) {
-    r.output_stream << "Goodbye for now!\n";
-    return FirstImpression::Stupendous;
-  }
+class SpeakerHello
+    : public holden::request_handler<SayHello> {
+public:
+    // Say hello on the provided output stream.
+    std::shared_ptr<void> handle(SayHello& r) override {
+        r.output_stream << "Hello, world!\n";
+        return nullptr;
+    }
+
+};
+
+class SpeakerGoodbye
+    : public holden::request_handler<SayGoodbye> {
+public:
+
+    // Say goodbye on the provided output stream, and return how the interaction 
+    // went.
+    std::shared_ptr<FirstImpression> handle(SayGoodbye& r) override {
+        r.output_stream << "Goodbye for now!\n";
+        return std::make_shared<FirstImpression>(FirstImpression::Stupendous);
+    }
 };
 
 
 
 int main() {
-  Speaker s{};
-  auto mediator = holden::make_mediator(s);
+    holden::mediator mediator;
 
-  SayHello say_hello{ std::cout };
-  mediator.send(say_hello);
+    auto sh = std::shared_ptr<SpeakerHello>(new SpeakerHello());
+    auto sg = std::shared_ptr<SpeakerGoodbye>(new SpeakerGoodbye());
+    
+    mediator.add_handler(sh);
+    mediator.add_handler(sg);
 
-  auto first_impression = mediator.send(SayGoodbye{std::cout});
+    SayHello say_hello{ std::cout };
+    mediator.send(say_hello);
 
-  std::cout << "The speaker's first impression was " << first_impression << "\n";
+    SayGoodbye say_goodbye{ std::cout };
+    auto first_impression = mediator.send(say_goodbye);
 
-  return 0;
+    std::cout << "The speaker's first impression was " << *first_impression << "\n";
+
+    return 0;
 }
-
-/* output:
- *
- * $ Hello, world!
- * $ Goodbye for now!
- * $ The speaker's first impression was studpendous!!!
-*/
--- include/cpp_mediator/mediator.hpp
+++ include/cpp_mediator/mediator.hpp
@@ -1,122 +1,67 @@
 #ifndef HOLDEN_MEDIATOR_HPP_
 #define HOLDEN_MEDIATOR_HPP_
 
-#include <cstddef>
-#include <tuple>
-#include <type_traits>
-#include <utility>
+#include <stdexcept>
+#include <vector>
+#include <memory>
 
 namespace holden {
 
-namespace detail {
-namespace tuple_searching {
+    // An optional pure-virtual struct to mark request types
+    template <typename TResponse>
+    struct request {
+        using response_type = TResponse;
+        virtual ~request() = default;
+    };
 
 
-template<class...>  struct voider { using type = void; };
-template<class...Ts> using void_t = typename voider<Ts...>::type;
+    struct request_handler_base {
+        virtual ~request_handler_base() = default;
+        virtual std::shared_ptr<void> _handle(void* request) { return nullptr; }
+    };
 
-template<template<class...> class Test, class Tuple>
-struct get_first_passing_type;
-template<template<class...> class Test, class Tuple>
-using get_first_passing_type_t
-  = typename get_first_passing_type<Test, Tuple>::type;
-
-template<template<class...> class, class, class...> struct first_passing_type;
-template<template<class...> class Test, class T0, class...Ts>
-struct first_passing_type<Test, std::enable_if_t<!Test<T0>::value>, T0, Ts...>
-  : first_passing_type<Test, void, Ts...>
-{};
-
-template <bool... Ts>
-struct any_of : public std::false_type {  };
-
-template <bool b, bool... Ts>
-struct any_of<b, Ts...>
-  : public std::conditional_t<b, std::true_type, any_of<Ts...>> {};
-
-template<template<class...> class Test, class T0, class...Ts>
-struct first_passing_type<Test, std::enable_if_t<Test<T0>::value>, T0, Ts...> {
-  using type = T0;
-  static_assert(!any_of<Test<Ts>::value...>::value, 
-                "multiple types are registered for a given request");
-};
-
-template<template<class...> class Test,
-  template<class...> class Tuple, class...Ts>
-struct get_first_passing_type<Test, Tuple<Ts...>>
-  : first_passing_type<Test, void, Ts...>
-{};
-
-template<class Base>
-struct is_derived_from {
-  template<class Derived>
-  using test = std::is_base_of<std::decay_t<Base>, std::decay_t<Derived>>;
-};
-
-template<class Base, class Tuple>
-using get_first_derived =
-  get_first_passing_type_t<is_derived_from<Base>::template test, Tuple>;
-
-template<class Base, class Tuple>
-auto get_from_base(Tuple&& tuple)
--> decltype(std::get< get_first_derived<Base,
-      std::decay_t<Tuple>> >(std::forward<Tuple>(tuple))) {
-  return std::get< get_first_derived<Base,
-      std::decay_t<Tuple>> >(std::forward<Tuple>(tuple));
-}
-
-} // namespace tuple_searching
-
-template<typename T> inline
-T& ref(T& x) { return x; }
-template<typename T> inline
-T& ref(T* x) { return *x; }
-
-} // namespace detail
-
-// An optional pure-virtual struct to mark request types
-template <typename TResponse>
-struct request {
-  using response_type = TResponse;
-  virtual ~request() = 0;
-};
-
-template <typename TResponse>
-request<TResponse>::~request() {}
+    template <typename TRequest>
+    struct request_handler : request_handler_base {
+        virtual ~request_handler() = default;
+        virtual std::shared_ptr<void> _handle(void* request) override {
+            return static_cast<std::shared_ptr<void>>(handle(*static_cast<TRequest*>(request)));
+        }
+        virtual std::shared_ptr<typename TRequest::response_type> handle(TRequest& request) = 0;
+    };
+    
 
 
-template <typename TRequest>
-struct request_handler {
-  virtual ~request_handler() = 0;
-};
+    class mediator {
+    protected:
+        std::vector<std::shared_ptr<request_handler_base>> handlers_;
 
-template <typename TRequest>
-request_handler<TRequest>::~request_handler() {}
+    public:
+        mediator() {}
 
+        template<typename THandler>
+        void add_handler(std::shared_ptr<THandler>& handler) {
+            std::shared_ptr<request_handler_base> hb = std::dynamic_pointer_cast<request_handler_base>(handler);
+            handlers_.push_back(hb);
+        }
 
-template <typename ...Handlers>
-class mediator {
- protected:
-  std::tuple<Handlers...> handlers_;
+        template<typename TRequest>
+        auto send(TRequest& r) -> std::shared_ptr < typename TRequest::response_type> {
+            for (std::shared_ptr<request_handler_base>& handler : handlers_) {
+                std::shared_ptr<request_handler<TRequest>> typed_handler = std::dynamic_pointer_cast<request_handler<TRequest>>(handler);
+                if (typed_handler) {
+                    return typed_handler->handle(r);
+                }
+            }
+            throw std::runtime_error("No handler found for request type");
+        }
 
- public:
-  mediator(Handlers... handlers) : handlers_(handlers...) {}
+        virtual ~mediator() {}
+    };
 
-  template<typename TRequest>
-  auto send(const TRequest& r) -> typename TRequest::response_type {
-    using namespace detail;
-    using namespace detail::tuple_searching;
-    using handler_t = request_handler<std::decay_t<TRequest>>;
-    return ref(get_from_base<handler_t>(handlers_)).handle(r);
-  }
-
-  virtual ~mediator() {}
-};
-
-template <typename... Args>
-mediator<Args&...> make_mediator(Args&... args) {
-  return mediator<Args&...>(args...);
-}
+    template <typename... Args>
+    mediator make_mediator(Args&... args) {
+        return mediator(args...);
+    }
 
 } // namespace holden
 
